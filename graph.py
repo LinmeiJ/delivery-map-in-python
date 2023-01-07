76492aeaa0a8fb15da8e3c76dadd557f5a2a3558
Linmeim <43623996+LinmeiJ@users.noreply.github.com>
2023-01-06 23:59:19 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -13,16 +13,18 @@ class Graph:
             for row in reader:
                 # The first row contains the addresses
                 if row[0] == "":
-                    vertexs = row[3:] #addresses
+                    vertexes = row[2:]  # addresses
                 else:
                     # The first column of each row contains the destination addresses
-                    destNode = row[0] + " " + row[1] + " " + row[2]
+                    dest_node = row[0] + " " + row[1]
                     # The remaining columns contain the distances to the destination addresses
-                    for address, distance in enumerate(row[3:]):  # addresses in first row in csv file
-                        # Get the destination address
-                        vertex = vertexs[address]
-                        # Add the edge to the dictionary: key is the starting address, value is a list of destination addresses and the distance from the starting address
+                    for i, distance in enumerate(row[2:]):  # addresses in first row in csv file
+                        # Get the destination address start from index 0
+                        vertex = vertexes[i]
+                        # Add the edge to the dictionary: key is the starting address, value is a dictionary of
+                        # destination addresses and distances from the starting address
                         if vertex not in self.edges:
-                            self.edges[vertex] = [(destNode, distance)]
+                            self.edges[vertex] = [(dest_node, distance)]
                         else:
-                            self.edges[vertex].append((destNode, distance))
+                            self.edges[vertex].append((dest_node, distance))
+
