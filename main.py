8d2cfa00bfdff0cdccfec0088ec50dd003bf07d2
Linmeim <43623996+LinmeiJ@users.noreply.github.com>
2023-01-09 00:14:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ pkgs = Package(0, '', '', '', '', '', '', '', '', '', False)  # Create an instan
 pkgs.load_package_data()
 # Load graph data from a csv file
 graph = Graph()
-# Initialize a truck object
-truck = Truck(pkgs.package_table)
 
-# set hub location
-start_location = '4001 South 700 East 84107'
+# set hub location with a package placeholder
+start_location = vars(Package(0, '4001 South 700 East', '', '', '84107', '', '', '', '', '', 'delivered'))
+
 
 def calc_distance(route1, route2):
     for kv in graph.address_with_distance.items():
@@ -21,58 +20,76 @@ def calc_distance(route1, route2):
                     return elem[1]
 
 
-# Fix me: this is just for finding the fastest route for truck1. it can be refactored and combined with other method
-# / can be fixed when there is time
-def get_address_from_truck(trk):
-    locations = []
-
-    # get a list of just address of all packages
-    for pkg in trk:
-        locations.append(pkg.get('address') + ' ' + pkg.get('zip_code'))
-
-    return locations
-
-
 # TSP algorithm for planning a route for a fast delivery
-def find_fastest_route(current_location, locations):
+def find_fastest_route(sta_location, loaded_truck, sta_time):
     # Initialize a route and assign current location to it
-    route = [current_location]
+    route = []
+    priority_pkgs = []
+    current_time = start_time
+    for pkg in loaded_truck:
+        if pkg.get('deadline') != 'EOD':
+            priority_pkgs.append(pkg)
+    # Plan a route for priority packages first
+    add_nearest_locations_to_route(priority_pkgs, route, sta_location, current_time)
+    # plan the route based on existing priory route
+    add_nearest_locations_to_route(loaded_truck, route, sta_location, current_time)
 
-    # Keep looping until all locations have been visited
-    while locations:
-        # Find the nearest unvisited location to the current location
-        nearest_location = None
-        nearest_distance = float('inf')
-        for location in locations:
-            if type(location) is dict:
-                address = location['address'] + ' ' + location['zip_code']
-            else:
-                address = vars(location[1]).get('address') + ' ' + vars(location[1]).get('zip_code')
-            location_distance = calc_distance(current_location, address)
-            if location_distance != '' and location_distance is not None:
-                dist = float(location_distance)
-                if dist < nearest_distance:
-                    nearest_location = location
-                    nearest_distance = dist
-
-        if nearest_location is not None:
-            # Add the nearest location to the route
-            route.append(nearest_location)
-            # Remove the nearest location from the list of unvisited locations
-            locations.remove(nearest_location)
-
-        # Set the current location to the nearest location
-        current_location = nearest_location
-        break
     # Return the final route
     return route
 
-truck.load_cargo(pkgs)
+
+def add_nearest_locations_to_route(loaded_truck, route, sta_location, sta_time):
+    # Keep looping until all locations have been visited
+    route.append(start_location)
+    while loaded_truck:
+        # Find the nearest unvisited location to the current location
+        nearest_location = None
+        nearest_distance = float('inf')
+        for pkg in route:
+            current_location = pkg.get('address') + ' ' + pkg.get('zip_code')
+
+        for pkg in loaded_truck:
+            if route.count(pkg) > 0:
+                continue
+            else:
+                address = pkg.get('address') + ' ' + pkg.get('zip_code')
+                location_distance = calc_distance(current_location, address)
+                if location_distance != '' and location_distance is not None:
+                    dist = float(location_distance)
+                    if dist < nearest_distance:
+                        nearest_location = pkg
+                        nearest_distance = dist
+        if nearest_location is not None:
+            # calc_delivery_time_by_distance(route, nearest_distance, start_time)  # fix me -->  need to calculate the time and delivery status, 
+            # Add the nearest location to the route
+            route.append(nearest_location)
+            # Remove the nearest location from the list of unvisited locations
+            loaded_truck.remove(
+                nearest_location)
+        # Set the current location to the nearest location
+        current_location = nearest_location
 
 
-route_plan_for_all_pkg = find_fastest_route(start_location, pkgs.all_package_info_list)
-route_for_urgent_delivery = find_fastest_route(start_location, pkgs.package_urgent_list)
-shortest_path_truck1 = find_fastest_route(start_location, get_address_from_truck(truck.truck1))
+# fast_route_plan_for_all_packages = find_fastest_route(start_location, pkgs.all_package_info_list)
+# fast_route_plan_for_all_packages.remove(start_location)  # remove the placeholder location from the list
+# print(fast_route_plan_for_all_packages)
+
+
+truck = Truck(pkgs.package_urgent_list, pkgs.package_urgent_delayed_list,
+              pkgs.package_not_urgent_delayed_list, pkgs.package_with_wrong_address, pkgs.package_with_truck2_only,
+              pkgs.package_must_on_same_truck,
+              pkgs.package_remaining_packages)
+truck.load_cargo()
+start_time = "8:00 AM"
+route_for_truck1 = find_fastest_route(start_location, truck.truck3, start_time)
+route_for_truck1.remove(start_location)   # remove the placeholder location from the list
+print(route_for_truck1)
+
+#
+# route_for_truck2 = find_fastest_route(start_location, truck.truck1)
+# route_for_truck2.remove(start_location)   # remove the placeholder location from the list
+# print(route_for_truck2)
+
 
 # print('======== WGUPS Routing Program =======')
 # ans = True
