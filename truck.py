4f7b46ad80a7f002cb5bfc7b5cfeabd287b4809a
Linmeim <43623996+LinmeiJ@users.noreply.github.com>
2023-01-12 22:23:05 -0500

    Update truck.py

--- truck.py
+++ truck.py
@@ -1,3 +1,9 @@
+def load_delayed_packages(truck, packages):
+    for pkg in packages:
+        truck.append(pkg)
+    return truck
+
+
 class Truck:
     # Initialize three empty trucks
     truck1 = []
@@ -23,11 +29,8 @@ class Truck:
 
     def load_cargo_by_urgency(self):
         size = len(self.package_urgent_list)
-        for i, pkg in enumerate(self.package_urgent_list):
-            if i < (size/2):
-                self.truck1.append(pkg)
-            else:
-                self.truck2.append(pkg)
+        for pkg in self.package_urgent_list:
+            self.truck1.append(pkg)
 
         # there are a list of packages are also consider as urgent packages, so load them along with truck1
         for pkg in self.package_must_on_same_truck:
@@ -37,15 +40,8 @@ class Truck:
     def load_cargo_by_remaining_packages(self):
         for pkg in self.package_with_truck2_only:
             self.truck2.append(pkg)
-        for pkg in self.package_urgent_delayed_list:
-            self.truck2.append(pkg)
-        for pkg in self.package_not_urgent_delayed_list:
-            if len(self.truck1) <= 16:
-                self.truck1.append(pkg)
-            else:
-                self.truck3.append(pkg)
         for pkg in self.package_with_wrong_address:
-            # Since there are only 2 drivers, load the wrong address to trucks3 as it is last one gonna leave the Hub
+            # Since there are only 2 drivers, load the wrong address to trucks3 as it is last one leaves the Hub
             self.truck3.append(pkg)
         for pkg in self.package_remaining_packages:
             if len(self.truck1) < 16:
@@ -55,9 +51,15 @@ class Truck:
             else:
                 self.truck3.append(pkg)
 
-
-    # Trucks travel at a speed of 18 miles/hr, leave hub no earlier than 8:00AM
-    # def calc_time_travel(self, travel_miles):  # fix me
-    #     pass
-
+    def load_delayed_packages(self, truck2, truck3):
+        for pkg in self.package_urgent_delayed_list:
+            if len(truck2) < 16:
+                truck2.append(pkg)
+            else:
+                truck3.append(pkg)
+        for pkg in self.package_not_urgent_delayed_list:
+            if len(truck2) < 16:
+                truck2.append(pkg)
+            else:
+                truck3.append(pkg)
 
